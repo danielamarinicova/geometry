357691e6f9f4cb1abca6edb8b3deea84eca78f63
Daniela Marinicova <marinicova.d@gmail.com>
2018-01-11 12:42:32 +0100

    geo
    geo

---
+++ src/sk/akademiasovy/geometry/Triangle.java
@@ -0,0 +1,76 @@
+package sk.akademiasovy.geometry;
+
+public class Triangle {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triangle() {
+        a=b=c=0;
+    }
+
+
+    public double getPerimeter() {
+        return a+b+c;
+    }
+
+    public double getArea(){
+        double s;
+        s=getPerimeter()/2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+
+    }
+
+    public boolean isRightAngled(){
+        if(a*a+b*b==c*c)
+            return true;
+        else if(a*a+c*c==b*b)
+            return true;
+        else if(b*b+c*c==a*a)
+            return true;
+        else
+            return false;
+
+
+
+    }
+
+    public boolean isExisting(){
+        if(a+b<c)
+            return true;
+        else if(a+c<b)
+            return true;
+        else if(b+c<a)
+            return true;
+        else
+            return false;
+
+    }
+
+    public boolean isEquilateral (){
+        if(a+b==c/2)
+            return true;
+        else
+            return false;
+
+    }
+
+    public boolean isIsosceles (){
+        if(a==b)
+            return true;
+        else if(a==c)
+            return true;
+        else if(b==c)
+            return true;
+        else
+            return false;
+    }
+
+}
--- src/sk/akademiasovy/main/Main.java
+++ src/sk/akademiasovy/main/Main.java
@@ -2,13 +2,14 @@ package sk.akademiasovy.main;
 
 import sk.akademiasovy.geometry.Rectangle;
 import sk.akademiasovy.geometry.Square;
+import sk.akademiasovy.geometry.Triangle;
 
 public class Main {
     public static void main(String[] args) {
         Square square1 = new Square(7.5);
         Square square2 = new Square();
         Rectangle rect1 = new Rectangle(18.5,11.15);
-
+        Triangle tri1 = new Triangle (3 , 4 ,5);
         System.out.println("Area of square is:" + square1.getArea() );
         System.out.println("Perimeter of square is:" + square1.getPerimeter() );
         System.out.println("Diagonal of square is:" + square1.getDiagonal() );
@@ -16,5 +17,12 @@ public class Main {
         System.out.println("Area of rectangle is:" + rect1.getArea() );
         System.out.println("Perimeter of rectangle is:" + rect1.getPerimeter() );
         System.out.println("Diagonal of rectangle is:" + rect1.getDiagonal() );
+
+        System.out.println("Perimeter of Triangle is:" + tri1.getPerimeter() );
+        System.out.println("Area of Triangle is:" + tri1.getArea() );
+        System.out.println("Is Triangle rightangled?:" + tri1.isRightAngled() );
+        System.out.println("Is Triangle existing?:" + tri1.isExisting() );
+        System.out.println("Is Triangle isosceles ?:" + tri1.isIsosceles() );
+        System.out.println("Is Triangle equilateral ?:" + tri1.isEquilateral() );
     }
 }
